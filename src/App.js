22da6457e9469901dcecfaddd9427ad4ba4be46b
kalcalen <kalcalen@gmail.com>
2020-06-30 10:54:05 -0700

    final

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style/master.scss"; // applies global scss styles
+
+function App() {
+   return (
+      <div>
+         <h1 className="text-primary">Hello World</h1>
+      </div>
+   );
+}
+
+export default App;
