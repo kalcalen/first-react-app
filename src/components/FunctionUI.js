282ee50cfd2caa9740e0e54f6b99954ebe43062a
kalcalen <kalcalen@gmail.com>
2020-07-04 05:41:37 -0700

    Converted Functional component to class component

--- src/components/FunctionUI.js
+++ src/components/FunctionUI.js
@@ -2,27 +2,27 @@ import React from "react";
 import utils from "../utils/utils";
 import { converDataType } from "../utils/helpers";
 
-export default function FunctionUI(props) {
-   console.log(utils.add);
+export default class FunctionUI extends React.Component {
+   constructor() {
+      super();
+      this.state = {
+         isResultDisplayed: false,
+         isCodeDisplayed: false,
+         result: "",
+      };
+   }
 
-   const renderInputs = (num) => {
-      const inputs = [];
-      for (let i = 0; i < num; i++) {
-         const id = `input- ${props.name} - ${i}`;
-         // render some JSX
-         inputs.push(
-            <input
-               type="text"
-               className="form-control inline-action"
-               key={id}
-               id={id}
-            />
-         );
-      }
-      return inputs;
-   };
+   toggleCodeDisplay() {
+      this.setState({ isCodeDisplayed: !this.state.isCodeDisplayed });
+      // if (this.state.isCodeDisplayed === false) {
+      //    this.setState({ isCodeDisplayed: true });
+      // } else {
+      //    this.setState({ isCodeDisplayed: false });
+      // }
+   }
 
-   function getUserInput() {
+   getUserInput() {
+      const props = this.props;
       console.log(props.name);
       const inputValues = [];
       for (let i = 0; i < props.inputs; i++) {
@@ -32,28 +32,60 @@ export default function FunctionUI(props) {
          inputValues.push(convertedValue);
       }
       console.log(inputValues);
+      const result = utils[props.name](...inputValues);
+      console.log(result);
+      this.setState({
+         result: JSON.stringify(result),
+         isResultDisplayed: true,
+      });
    }
-   return (
-      <div className="col-12 col-lg-8 offset-lg-2 mb-5">
-         <p className="name">
-            <b>{props.name}</b> &nbsp;- &nbsp;
-            {props.desc}
-         </p>
-         <pre style={{ display: "none" }}>
-            <code></code>
-         </pre>
-         <div className="actions float-right">
-            {renderInputs(props.inputs)}
-            <button
-               className="btn btn-primary inline-action"
-               onClick={() => getUserInput()}
-            >
-               Run
-            </button>
+
+   render() {
+      const props = this.props;
+
+      const renderInputs = (num) => {
+         const inputs = [];
+         for (let i = 0; i < num; i++) {
+            const id = `input- ${props.name} - ${i}`;
+            // render some JSX
+            inputs.push(
+               <input
+                  type="text"
+                  className="form-control inline-action"
+                  key={id}
+                  id={id}
+               />
+            );
+         }
+         return inputs;
+      };
+
+      return (
+         <div className="col-12 col-lg-8 offset-lg-2 mb-5">
+            <p className="name" onClick={() => this.toggleCodeDisplay()}>
+               <b>{props.name}</b> &nbsp;- &nbsp;
+               {props.desc}
+            </p>
+            {this.state.isCodeDisplayed && (
+               <pre>
+                  <code>{String(utils[props.name])}</code>
+               </pre>
+            )}
+
+            <div className="actions float-right">
+               {renderInputs(props.inputs)}
+               <button
+                  className="btn btn-primary inline-action"
+                  onClick={() => this.getUserInput()}
+               >
+                  Run
+               </button>
+            </div>
+            <div className="clearfix mb-3"></div>
+            {this.state.isResultDisplayed && (
+               <div className="alert alert-primary">{this.state.result}</div>
+            )}
          </div>
-         <div className="clearfix mb-3"></div>
-         <div className="alert alert-primary" style={{ display: "none" }}></div>
-         <div className="alert alert-danger" style={{ display: "none" }}></div>
-      </div>
-   );
+      );
+   }
 }
